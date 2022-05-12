da236f34e15115e83e70631e28544c89eec94eec
Amitai Gottlieb <amitaig@jfrog.com>
2022-05-12 18:12:20 +0300

    urgency

--- monday/test.py
+++ monday/test.py
@@ -1,4 +1,4 @@
 import api
 
-api.create_task(2664551840, "7000", "a ticket", "some description of a ticket")
+api.create_task(2664551840, "7003", "a ticket", "some description of a ticket", urgency=1)
 # print(api.get_buildings())
